abe97fc868e88f9ae1d745b2b70a6b2e28ae78c0
elestranobaron <tll@LAPTOP-5Q7RHMNT>
2025-08-31 22:20:19 +0200

    Ported System.cpp to Linux, removed StdAfx.h dependency, updated CMakeLists.txt with MySQL, SDL3, OpenSSL, ODBC

--- T4C Server/System.cpp
+++ T4C Server/System.cpp
@@ -1,6 +1,17 @@
-#include "StdAfx.h"
-#ifdef WIN32
-	#include "System-Win32.cpp"
-#else
-	#include "System-Linux.cpp"
-#endif
+#include "System.h"
+#include <string>
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <errno.h>
+
+std::string System::GetMachineName() {
+    char hostname[256];
+    if (gethostname(hostname, sizeof(hostname)) == -1) {
+        std::cerr << "Error getting hostname: " << strerror(errno) << std::endl;
+        return "unknown";
+    }
+    return std::string(hostname);
+}
